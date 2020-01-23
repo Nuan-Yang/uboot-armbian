56a90aef393a7d3c2865fd2b8efd521f68a69b51
Lin <767763591@qq.com>
2020-01-23 02:08:56 +0000

    Add compile.sh

---
+++ compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d toolchain ]; then
+    echo "Decompressing Toolchain..."
+    mv toolchain.tar toolchain.tar.xz
+    tar xf toolchain.tar.xz
+    if [ $? != 0 ]; then
+        echo Please download toolchain.tar from https://www.lanzous.com/i8tqw3i
+        exit 1
+    fi
+fi
+
+export PATH=$PATH:/`pwd`/toolchain/Sourcery_G++_Lite/bin 
+make m8b_m201_1G_config
+make -j`nproc` firmware
+echo "********************"
+echo "tools/convert --soc m8b -s build/firmware.bin -i build/u-boot-comp.bin -o build/u-boot.bin"
+echo "***Firmware Built***"
